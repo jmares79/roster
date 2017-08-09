2da4e60c0b9a7825edacd61e0e40a265b96338cb
jmares79 <jmares79@gmail.com>
2017-08-09 11:17:49 -0300

    -Deleted test DDBB, as it is not used yet +) League generator test

--- tests/RosterBundle/Service/CommonLeagueGeneratorTest.php
+++ tests/RosterBundle/Service/CommonLeagueGeneratorTest.php
@@ -11,14 +11,21 @@ class CommonLeagueGeneratorTest extends TestCase
 {
     public function setUp()
     {
+        $mockedBot = $this->createMock(Bot::class);
         $mockedBotGenerator = $this->createMock(CommonBotGenerator::class);
         $mockedEntityManager = $this->createMock(EntityManager::class);
+
+        $mockedBotGenerator
+            ->method('generate')
+            ->willReturn($mockedBot);
 
         $this->commonLeagueGenerator = new CommonLeagueGenerator($mockedBotGenerator, $mockedEntityManager);
     }
 
     public function testGenerateLeague()
     {
+
+        $league = $this->commonLeagueGenerator->generateLeague();
 
     }
 }
